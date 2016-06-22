32e127f39a83e3439434fd9479df1348c32d2e2d
khanfusiion3 <khanfusiion3@gmail.com>
2016-06-22 00:20:07 -0700

    Update Shape.h

--- Shape.h
+++ Shape.h
@@ -81,34 +81,7 @@ struct Circle : public Shape
 
   void Draw( void ) const
   {
-    const uint32 k_segments = 20;
-
-    // Render a circle with a bunch of lines
-    glColor3f( body->r, body->g, body->b );
-    glBegin( GL_LINE_LOOP );
-    real theta = body->orient;
-    real inc = PI * 2.0f / (real)k_segments;
-    for(uint32 i = 0; i < k_segments; ++i)
-    {
-      theta += inc;
-      Vec2 p( std::cos( theta ), std::sin( theta ) );
-      p *= radius;
-      p += body->position;
-      glVertex2f( p.x, p.y );
-    }
-    glEnd( );
-
-    // Render line within circle so orientation is visible
-    glBegin( GL_LINE_STRIP );
-    Vec2 r( 0, 1.0f );
-    real c = std::cos( body->orient );
-    real s = std::sin( body->orient );
-    r.Set( r.x * c - r.y * s, r.x * s + r.y * c );
-    r *= radius;
-    r = r + body->position;
-    glVertex2f( body->position.x, body->position.y );
-    glVertex2f( r.x, r.y );
-    glEnd( );
+    return;
   }
 
   Type GetType( void ) const
@@ -186,14 +159,7 @@ struct PolygonShape : public Shape
 
   void Draw( void ) const
   {
-    glColor3f( body->r, body->g, body->b );
-    glBegin( GL_LINE_LOOP );
-    for(uint32 i = 0; i < m_vertexCount; ++i)
-    {
-      Vec2 v = body->position + u * m_vertices[i];
-      glVertex2f( v.x, v.y );
-    }
-    glEnd( );
+    return;
   }
 
   Type GetType( void ) const
